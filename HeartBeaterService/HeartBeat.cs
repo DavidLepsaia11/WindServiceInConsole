1cd25b757b4c586942b45e5dfb63427005b1f3ea
David Lepsaia <davidlepsaia1121@gmail.com>
2024-08-14 02:11:10 +0400

    Add project files.

---
+++ HeartBeaterService/HeartBeat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
+
+namespace HeartBeaterService
+{
+    public class HeartBeat
+    {
+        private readonly Timer _timer;
+        private readonly string _filePath;
+        public HeartBeat()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _filePath = @"D:\test.txt";
+            _timer.Elapsed += TimerElapsedEv;
+        }
+
+        private void TimerElapsedEv(object sender, ElapsedEventArgs e)
+        {
+            string[] lines = new string[] { DateTime.Now.ToString() };
+            File.AppendAllLines(_filePath,lines);
+        }
+
+        public void Start() 
+        { 
+          _timer.Start();   
+        }
+
+        public void Stop() 
+        {
+          _timer.Stop();
+        }
+    }
+}
---
+++ HeartBeaterService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace HeartBeaterService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var exitCode = HostFactory.Run(x => 
+            {
+                x.Service<HeartBeat>(s => 
+                {
+                    s.ConstructUsing(heartBeat => new HeartBeat());
+                    s.WhenStarted(heartBeat => { heartBeat.Start();});
+                    s.WhenStopped(heartBeat => { heartBeat.Stop();});
+                });
+                x.RunAsLocalService();
+                x.SetServiceName("HeartBeatService");
+                x.SetDescription("Heart Beat service sample");
+            });
+
+            int exitCodeValue = (int)Convert.ChangeType(exitCode,exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
+    }
+}
